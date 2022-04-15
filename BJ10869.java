7747889083a7b2ca015c760e6e18ad94c8887f64
gardenee <snnjjang@naver.com>
2022-04-15 10:28:33 +0900

    220415

---
+++ BJ10869.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class BJ10869 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+
+    }
+}
