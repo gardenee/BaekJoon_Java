8811d9f29bf58949ece82d102c448200d6272789
garden <dalkomjjang@gmail.com>
2022-04-20 15:41:35 +0900

    220420

---
+++ BJ2914.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class BJ2914 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int I = sc.nextInt();
+
+        System.out.println(A * (I -1) + 1);
+
+    }
+}
