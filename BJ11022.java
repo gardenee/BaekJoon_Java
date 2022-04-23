2cf5128e328d0251a3a9d71631ec3ea6fabae2f0
gardenee <snnjjang@naver.com>
2022-04-23 17:12:46 +0900

    220423 반복문

---
+++ BJ11022.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BJ11022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println("Case #" + (i+1) + ": " + A + " + " + B + " = " + (A+B));
+        }
+    }
+}
---
+++ BJ15552.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ15552 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            bw.write((Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken()))+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
