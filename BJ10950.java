2fdf505d97e617fc2221c4be35feae5da268b7ad
gardenee <snnjjang@naver.com>
2022-04-23 14:18:08 +0900

    220423 반복문

---
+++ BJ10950.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BJ10950 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
