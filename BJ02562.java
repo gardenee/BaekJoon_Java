a293283f8495997b1666ee796436a6584fd396b2
gardenee <snnjjang@naver.com>
2022-04-24 13:29:51 +0900

    220423 1차원배열

--- BJ02562.java
+++ BJ02562.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class BJ02562 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = sc.nextInt();
+        int index = 1;
+
+        for (int i = 2; i <= 9 ; i++) {
+            int curr = sc.nextInt();
+            if (curr > max) {
+                max = curr;
+                index = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
 }
