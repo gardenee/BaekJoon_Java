77c98a8efb5f6b6dc6812543eb26f8feec67a490
gardenee <snnjjang@naver.com>
2022-04-29 23:58:38 +0900

    220428

--- BJ01049.java
+++ BJ01049.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class BJ01049 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
