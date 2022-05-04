e71395a887b8c0ef666aaa3071b610864cee964d
gardenee <snnjjang@naver.com>
2022-05-04 23:53:20 +0900

    220504 스터디3

--- BJ01417.java
+++ BJ01417.java
@@ -3,7 +3,6 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class BJ01417 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
