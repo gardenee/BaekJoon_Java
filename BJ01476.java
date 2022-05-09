9ee397f31959be75f3c9a4089e2b7a3824e014bd
gardenee <snnjjang@naver.com>
2022-05-09 13:39:38 +0900

    220509 스터디3

---
+++ BJ01476.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class BJ01476 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int E = sc.nextInt();
+        int S = sc.nextInt();
+        int M = sc.nextInt();
+
+        int e = 1;
+        int s = 1;
+        int m = 1;
+        int i = 1;
+
+        while (E != e || S != s || M != m) {
+            e++;
+            s++;
+            m++;
+            i++;
+
+            if (e == 16) {
+                e = 1;
+            }
+            if (s == 29) {
+                s = 1;
+            }
+            if (m == 20) {
+                m = 1;
+            }
+        }
+        System.out.println(i);
+    }
+}
