3c6f4106cda816b3fffbc1468a0ab1092fd43214
gardenee <snnjjang@naver.com>
2022-05-09 12:19:09 +0900

    220509 스터디3

---
+++ BJ02947.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BJ02947 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4-i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                    print(arr);
+                }
+            }
+        }
+    }
+
+    public static void print(int[] arr) {
+        for (int i: arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
