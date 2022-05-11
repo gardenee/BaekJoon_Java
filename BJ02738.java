f01a1115e5b91e3c8e22d4897169ada2feb2d72a
gardenee <snnjjang@naver.com>
2022-05-11 13:11:13 +0900

    220511 스터디4

---
+++ BJ02738.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class BJ02738 {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] answer = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                answer[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print((answer[i][j] + sc.nextInt()) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
