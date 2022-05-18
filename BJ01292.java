7798e58c791044b4b67ace2b15a75cb46f978ec8
gardenee <snnjjang@naver.com>
2022-05-18 15:37:22 +0900

    220518 스터디7

--- BJ01292.java
+++ BJ01292.java
@@ -7,7 +7,7 @@ public class BJ01292 {
         int A = sc.nextInt();
         int B = sc.nextInt();
 
-        int[] sum = new int[B+1];
+        int[] sum = new int[B+1]; // sum[i]는 수열의 1~i번째 수의 합을 저장
         int cnt = 0;
         int n = 1;
         for (int i = 1; i <= B; i++) {
