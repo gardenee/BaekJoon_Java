b74792dce1f66f874cddefbed37a37858d779c52
gardenee <snnjjang@naver.com>
2022-05-18 15:35:15 +0900

    220518 스터디7

--- BJ01292.java
+++ BJ01292.java
@@ -1,26 +1,24 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class BJ01292 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
 
-        int start = sc.nextInt();
-        int end = sc.nextInt();
-        int sum = 0;
-        int count = 0;
-        int i = 0;
-        while (count <= end) {
-            i++;
-            for (int j = 0; j < i; j++) {
-                count++;
-                if (start <= count && count <= end) {
-                    sum += i;
-                }
-                if (count > end) {
-                    break;
-                }
+        int[] sum = new int[B+1];
+        int cnt = 0;
+        int n = 1;
+        for (int i = 1; i <= B; i++) {
+            sum[i] += sum[i-1];
+            sum[i] += n;
+            cnt++;
+            if (cnt == n) {
+                n++;
+                cnt = 0;
             }
         }
-        System.out.println(sum);
+        System.out.println(sum[B]-sum[A-1]);
     }
 }
