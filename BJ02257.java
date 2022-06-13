e52d30501c5fd54a664e7c23c6fbe569d4959ad0
gardenee <snnjjang@naver.com>
2022-06-13 09:51:42 +0900

    220613 스터디14

---
+++ BJ02257.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class BJ02257 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Deque<String> stack = new ArrayDeque<>(Arrays.asList(sc.nextLine().split("")));
+        Deque<Integer> num = new ArrayDeque<>();
+        int ans = 0;
+        int temp = 1;
+        int mul = 1;
+
+        while (!stack.isEmpty()) {
+            String curr = stack.removeLast();
+            if (curr.equals("C")||curr.equals("H")||curr.equals("O")) {
+                if (temp != 1) {
+                    ans += atom(curr) * mul * temp;
+                    temp = 1;
+                }
+                else ans += atom(curr) * mul;
+            } else if (curr.equals(")")) {
+                num.addLast(temp);
+                mul *= temp;
+                temp = 1;
+            } else if (curr.equals("(")) {
+                mul /= num.removeLast();
+            } else {
+                temp = Integer.parseInt(curr);
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static int atom(String str) {
+        if (str.equals("C")) return 12;
+        if (str.equals("H")) return 1;
+        else return 16;
+    }
+}
--- BJ16173.java
+++ BJ16173.java
@@ -1,39 +1,59 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class BJ16173 {
 
+    public static int[] curr = {0, 0};
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Deque<String> stack = new ArrayDeque<>(Arrays.asList(sc.nextLine().split("")));
-        Deque<Integer> num = new ArrayDeque<>();
-        int ans = 0;
-        int temp = 1;
-        int mul = 1;
+        int N = sc.nextInt();
+        int[][] map = new int[N][N];
 
-        while (!stack.isEmpty()) {
-            String curr = stack.removeLast();
-            if (curr.equals("C")||curr.equals("H")||curr.equals("O")) {
-                if (temp != 1) {
-                    ans += atom(curr) * mul * temp;
-                    temp = 1;
-                }
-                else ans += atom(curr) * mul;
-            } else if (curr.equals(")")) {
-                num.addLast(temp);
-                mul *= temp;
-                temp = 1;
-            } else if (curr.equals("(")) {
-                mul /= num.removeLast();
-            } else {
-                temp = Integer.parseInt(curr);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                map[i][j] = sc.nextInt();
             }
         }
-        System.out.println(ans);
+
+        boolean[][] visited = new boolean[N][N];
+
+        boolean flag = true;
+        int[] temp;
+        Stack<int[]>  stack = new Stack<>();
+        stack.add(curr);
+
+        do {
+            visited[x()][y()] = true;
+            int val = map[x()][y()];
+
+            if (x()+val < N && !visited[x()+val][y()]) {
+                temp = new int[]{x() + val, y()};
+                stack.add(temp);
+                if (x()+val == N-1 && y() == N-1) flag = false;
+            }
+            if (y()+val < N && !visited[x()][y()+val]) {
+                temp = new int[]{x(), y() + val};
+                stack.add(temp);
+                if (x() == N-1 && y()+val == N-1) flag = false;
+            }
+            if (stack.isEmpty()) break;
+            do {
+                temp = stack.pop();
+            } while (!stack.isEmpty() && visited[temp[0]][temp[1]]);
+            curr = temp;
+
+        } while (!visited[x()][y()] || flag);
+
+        if (!flag) System.out.println("HaruHaru");
+        else System.out.println("Hing");
     }
 
-    public static int atom(String str) {
-        if (str.equals("C")) return 12;
-        if (str.equals("H")) return 1;
-        else return 16;
+    public static int x() {
+        return curr[0];
+    }
+
+    public static int y() {
+        return curr[1];
     }
 }
