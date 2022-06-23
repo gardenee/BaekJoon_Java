0812468fa1b5e227b5b31edcf37a1309eb339beb
gardenee <snnjjang@naver.com>
2022-06-24 00:22:34 +0900

    220624 스터디17

---
+++ BJ09372.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ09372 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for (int j = 0; j < M; j++) br.readLine();
+            System.out.println(N-1);
+        }
+    }
+}
