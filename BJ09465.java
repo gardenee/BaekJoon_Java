064a34be69ff99ea376ba8f8e71403bf086a37b5
gardenee <snnjjang@naver.com>
2022-07-13 20:58:09 +0900

    220713

--- BJ09465.java
+++ BJ09465.java
@@ -17,16 +17,13 @@ public class BJ09465 {
             st = new StringTokenizer(sc.nextLine());
             for (int i = 0; i < N; i++) stamps[i][1] = Integer.parseInt(st.nextToken());
 
-            if (N > 1) {
-                stamps[1][0] += stamps[0][1];
-                stamps[1][1] += stamps[0][0];
-            }
+            if (N > 1) stamps[1][0] += stamps[0][1];
+            if (N > 1) stamps[1][1] += stamps[0][0];
 
             for (int i = 2; i < N; i++) {
                 stamps[i][0] += Math.max(stamps[i-1][1], stamps[i-2][1]);
                 stamps[i][1] += Math.max(stamps[i-1][0], stamps[i-2][0]);
             }
-
             System.out.println(Math.max(stamps[N-1][0], stamps[N-1][1]));
         }
     }
