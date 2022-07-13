d7de9c829bf33403240c7f699b34660dbf54d04d
gardenee <snnjjang@naver.com>
2022-07-13 20:13:33 +0900

    220713 스터디 21

---
+++ BJ01027.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class BJ01027 {
+
+    public static double slope;
+    public static int temp;
+    public static int[] buildings;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+
+        buildings = new int[N];
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        for (int i = 0; i < N; i++) buildings[i] = Integer.parseInt(st.nextToken());
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            temp = 0;
+            slope = 1000000001;
+            for (int j = i-1; j >= 0; j--) {
+                double curr = (buildings[j]-buildings[i])/(double)(j-i);
+                if (curr < slope) {
+                    temp++;
+                    slope = curr;
+                }
+            }
+            slope = -1000000001;
+            for (int j = i+1; j < N; j++) {
+                double curr = (buildings[j]-buildings[i])/(double)(j-i);
+                if (curr > slope) {
+                    temp++;
+                    slope = curr;
+                }
+            }
+            if (temp > answer) answer = temp;
+        }
+        System.out.println(answer);
+    }
+}
